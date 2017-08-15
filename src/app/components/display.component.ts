33f90f5cf6d5164055cc090f847cff7064e376e1
Trisha C N <trisha.natanael@gmail.com>
2017-08-15 14:54:37 +0800

    tuepm3

--- src/app/components/display.component.ts
+++ src/app/components/display.component.ts
@@ -9,7 +9,7 @@ import { Card } from '../model/uno';
 })
 export class DisplayComponent implements OnInit {
 
-   @Input() contents: Players[];
+   /*@Input() contents: Players[]; */
 
   constructor() { }
 
