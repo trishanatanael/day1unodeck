46a4794dee3c9e3f99925fb717e0bd3333010dd4
Trisha C N <trisha.natanael@gmail.com>
2017-08-15 10:03:11 +0800

    tuesday1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header.component';
+import { DisplayComponent } from './components/display.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DisplayComponent
   ],
   imports: [
     BrowserModule
